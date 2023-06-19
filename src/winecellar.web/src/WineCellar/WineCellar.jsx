2f0a7ca624ba30b56a728bc2050c251439ec246f
Valentina <valentinagalyam@gmail.com>
2023-06-19 11:53:00 +0200

    wine cellar cards

--- src/winecellar.web/src/WineCellar/WineCellar.jsx
+++ src/winecellar.web/src/WineCellar/WineCellar.jsx
@@ -1,5 +1,7 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import React from "react";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
@@ -8,39 +10,32 @@ function WineCellar({ wines }) {
     <div>
       <h1>Wine Cellar</h1>
       <div className="wine-list">
-        {wines.map((wine) => (
-          <Card key={wine.id} style={{ width: "18rem" }}>
-            <Card.Img
-              variant="top"
-              src="https://as2.ftcdn.net/v2/jpg/00/63/14/57/1000_F_63145794_cND2i7uzm0cvOixQrODxSn9YT8Fia7J3.jpg"
-            />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-
-              <Link to={`/wine/${wine.id}`}>
-                <Button variant="primary">Go somewhere</Button>
-              </Link>
-            </Card.Body>
-          </Card>
-        ))}
+        <Row xs={1} md={4} className="g-4">
+          {wines.map((wine) => (
+            <Col key={wine.id}>
+              <Card>
+                <Card key={wine.id} style={{ width: "10rem" }} />
+                <Card.Img
+                  className="imgWineCard"
+                  variant="top"
+                  src={wine.imageURL}
+                />
+                <Card.Body>
+                  <Card.Title>
+                    {wine.name} {wine.wineMaker}
+                  </Card.Title>
+                  <Card.Text>{wine.year}</Card.Text>
+                  <Link to={`/wine/${wine.id}`}>
+                    <Button variant="primary">Details</Button>
+                  </Link>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
       </div>
     </div>
   );
 }
 
 export default WineCellar;
-
-// <div key={wine.id}>
-//             <Link to={`/wine/${wine.id}`}>
-//               <h2>{wine.name}</h2>
-//               <h2>{wine.winemaker}</h2>
-//               <h3>{wine.year}</h3>
-//               <h3>{wine.rating}</h3>
-//               <p>{wine.description}</p>
-//               <p>{wine.tags}</p>
-//             </Link>
-//           </div>
